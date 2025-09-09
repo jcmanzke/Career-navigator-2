6aa308ede40a22e9bfddafc86e378053b86c5170
jcmanzke <jcmanzke@gmail.com>
2025-09-09 16:50:11 +0200

    chore(auto): main — 2025-09-09 16:50

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: false },
+  typedRoutes: false,
   typescript: { ignoreBuildErrors: true }
 };
 export default nextConfig;
