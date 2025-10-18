ebeff7833710ca16a20c1cb270de03f173ee37da
jcmanzke <jcmanzke@gmail.com>
2025-10-18 13:21:36 +0200

    chore(main): update

--- lib/n8n.ts
+++ lib/n8n.ts
@@ -1,5 +1,5 @@
 export const N8N_WEBHOOK_URL = "https://chrismzke.app.n8n.cloud/webhook/4646f17e-7ee3-40b8-b78e-fe9c59d31620";
 export const CONTEXT_HEADER_NAME = "Context-Identification";
 export const FAST_TRACK_STEP1_CONTEXT = "Fast track 1";
-export const FAST_TRACK_STEP3_CONTEXT = "Fast track 3";
+export const FAST_TRACK_STEP3_CONTEXT = "Fast Track 3";
 export const DEEP_ANALYSIS_CONTEXT = "Deep analysis";
